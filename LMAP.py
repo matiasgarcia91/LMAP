12cf4d4754d3ac469b8f2f3f2f9c7af9572b8c41
Matias Garcia <matiasigarcia91@gmail.com>
2018-10-24 22:57:28 +0200

    WIP bitarrays para ks y IDS

--- LMAP.py
+++ LMAP.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+from operator import xor
+from bitarray import bitarray
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,6 +11,28 @@ server_address = ('', 10010)
 sock.bind(server_address)
 print >>sys.stderr, 'starting up on %s port %s' % sock.getsockname()
 sock.listen(1)
+
+TAG_IDS = bitarray(96)
+TAG_IDS.setall(1);
+k1 = bitarray(96);
+k2 = bitarray(96);
+k3 = bitarray(96);
+k4 = bitarray(96);
+db = { TAG_IDS.to01(): { "k1": k1, "k2": k2, "k3": k3, "k4": k4 } }
+
+def calculateABC(IDS):
+    IDSArray = bitarray(IDS)
+    keys = db[IDS]
+    k1 = keys["k1"]
+    k2 = keys["k2"]
+    k3 = keys["k3"]
+    n1 = bitarray(96);
+    n2 = bitarray(96);
+    A = IDSArray ^ k1 ^ n1
+    B = (IDSArray | k2) + n1
+    C = IDSArray + k3 + n2
+    return [ A, B, C, n1, n2 ]
+
 
 while True:
     print >>sys.stderr, 'waiting for a connection'
@@ -20,11 +44,16 @@ while True:
             # Calculate A, B, C and send
             # Expect D, extract ID
             # Update IDS and Ks
-            data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
+            data = connection.recv(96)
+            todo = calculateABC(data)
+            print >>sys.stderr, 'received "%s"' % todo[0]
             if data:
                 connection.sendall(data)
             else:
                 break
     finally:
         connection.close()
+
+# A = IDS XOR K1 XOR n1 now the tag knows n1
+# B = (IDS OR K2) + n1 reader authentication
+# C = IDS + K3 + n2 the tag knows n2
--- tag.py
+++ tag.py
@@ -1,14 +1,18 @@
  #!/usr/bin/env python
 import socket
+from bitarray import bitarray
 
 TCP_IP = '0.0.0.0'
 TCP_PORT = 10010
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
+IDS = bitarray(96)
+IDS.setall(1);
 ID = "secreto"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
+
 while True:
     ready = raw_input("Type 'y' to begin")
     if ready == 'y':
@@ -17,7 +21,7 @@ while True:
         # Calculate B' and check it B == B'
         # Calculate and send D
         # Calculate new IDS, Ks 1-4 ,
-        s.send(MESSAGE)
+        s.send(IDS.to01())
         data = s.recv(BUFFER_SIZE)
         print "received data:", data
 
