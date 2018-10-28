3b57de6927977f945b1053f21024c9a13c7357b3
Matias Garcia <matiasigarcia91@gmail.com>
2018-10-28 19:32:40 +0100

    Add value updates, full protocol working

--- LMAP.py
+++ LMAP.py
@@ -1,14 +1,10 @@
-import socket
-import sys
-from operator import xor
-import random
-import json
+import socket, sys, random, json
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the address given on the command line
-server_address = ('', 10010)
+server_address = ('', 3001)
 sock.bind(server_address)
 print >>sys.stderr, 'starting up on %s port %s' % sock.getsockname()
 sock.listen(1)
@@ -16,7 +12,7 @@ sock.listen(1)
 initialized = False
 
 # Initial Ks and IDS should be shared between tag and reader
-IDS = random.randint(0, 2**96 - 1)
+IDS_init = random.randint(0, 2**96 - 1)
 k1 = random.randint(0, 2**96 - 1)
 k2 = random.randint(0, 2**96 - 1)
 k3 = random.randint(0, 2**96 - 1)
@@ -24,11 +20,13 @@ k4 = random.randint(0, 2**96 - 1)
 db = { "k1": k1, "k2": k2, "k3": k3, "k4": k4 }
 
 def calculateABC(IDS):
+    global db
     k1 = db["k1"]
     k2 = db["k2"]
     k3 = db["k3"]
     n1 = random.randint(0, 2**96 - 1)
     n2 = random.randint(0, 2**96 - 1)
+    db['IDS'] = IDS
     db['n1'] = n1
     db['n2'] = n2
     A = IDS ^ k1 ^ n1
@@ -37,9 +35,28 @@ def calculateABC(IDS):
     return [ A, B, C, n1, n2 ]
 
 def decodeD(D):
+    global db
     n1 = db['n1']
     n2 = db['n2']
+    IDS = db['IDS']
     ID = (D ^ n1 ^ n2) - IDS
+    return ID
+
+def update_values(ID):
+    global db
+    IDS = db['IDS']
+    k1 = db['k1']
+    k2 = db['k2']
+    k3 = db['k3']
+    k4 = db['k4']
+    n1 = db['n1']
+    n2 = db['n2']
+    newIDS = (IDS + (n2 ^ k4)) ^ ID
+    newk1 = k1 ^ n2 ^ (k3 + ID)
+    newk2 = k2 ^ n2 ^ (k4 + ID)
+    newk3 = (k3 ^ n1) + (k1 ^ ID)
+    newk4 = (k4 ^ n1) + (k2 ^ ID)
+    db = { 'k1': newk1, 'k2': newk2, 'k3': newk3, 'k4': newk4, 'IDS': newIDS }
     print ID
 
 while True:
@@ -47,12 +64,12 @@ while True:
     connection, client_address = sock.accept()
     try:
         print >>sys.stderr, 'client connected:', client_address
+        initialized = False
         while True:
             # Send initial IDS and Ks to tag - not part of the protocol but needed to share the
             # same initial values between entities
-            initialized = False
             if not initialized:
-                init = json.dumps({ 'IDS': IDS, 'k1': k1, 'k2': k2, 'k3': k3 })
+                init = json.dumps({ 'IDS': IDS_init, 'k1': k1, 'k2': k2, 'k3': k3, 'k4': k4 })
                 connection.sendall(init)
                 ack = connection.recv(10000)
                 if ack == 'ACK': initialized = True
@@ -64,16 +81,15 @@ while True:
             # Calculate A, B, C and send
             todo = calculateABC(int(ids_recieved['IDS']))
             abc = { 'A': todo[0], 'B': todo[1], 'C': todo[2] }
-            print { 'n1': todo[3], 'n2': todo[4] }
             abcSerial = json.dumps(abc)
             connection.sendall(abcSerial)
 
             # Expect D, extract ID
             authreplyraw = connection.recv(10000)
             authreply = json.loads(authreplyraw)
-            decodeD(authreply['D'])
-            # Update IDS and Ks
-            break
+            ID = decodeD(authreply['D'])
+            # Update IDS and Ks 1-4
+            update_values(ID)
     except Exception as e:
         print(e)
         connection.close()
--- tag.py
+++ tag.py
@@ -1,14 +1,12 @@
  #!/usr/bin/env python
-import socket, sys
-import random
-import json
+import socket, sys, random, json
 
 TCP_IP = '0.0.0.0'
-TCP_PORT = 10010
+TCP_PORT = 3001
 BUFFER_SIZE = 10000
 MESSAGE = "Hello, World!"
 ID = random.randint(0, 2**96 - 1)
-init = {}
+db = {}
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
@@ -16,12 +14,13 @@ initialized = False
 
 
 def verifyABC(abc):
+    global db
     A = abc['A']
     C = abc['C']
-    IDS = init['IDS']
-    k1 = init['k1']
-    k2 = init['k2']
-    k3 = init['k3']
+    IDS = db['IDS']
+    k1 = db['k1']
+    k2 = db['k2']
+    k3 = db['k3']
     n1 = A ^ k1 ^ IDS
     B2 = (IDS | k2) + n1
     if B2 == abc['B']:
@@ -29,30 +28,48 @@ def verifyABC(abc):
     else:
         print 'auth failed'
     n2 = C - IDS - k3
-    init['n1'] = n1
-    init['n2'] = n2
+    db['n1'] = n1
+    db['n2'] = n2
     D = (IDS + ID) ^ n1 ^ n2
+    print 'verifyready'
+    print db
     return D
 
-
+def update_values():
+    global db
+    IDS = db['IDS']
+    print 'updatedstarted'
+    k1 = db['k1']
+    k2 = db['k2']
+    k3 = db['k3']
+    k4 = db['k4']
+    n1 = db['n1']
+    n2 = db['n2']
+    print 'updatedadasd'
+    newIDS = (IDS + (n2 ^ k4)) ^ ID
+    newk1 = k1 ^ n2 ^ (k3 + ID)
+    newk2 = k2 ^ n2 ^ (k4 + ID)
+    newk3 = (k3 ^ n1) + (k1 ^ ID)
+    newk4 = (k4 ^ n1) + (k2 ^ ID)
+    db = { 'k1': newk1, 'k2': newk2, 'k3': newk3, 'k4': newk4, 'IDS': newIDS }
 
 try:
     while True:
         if not initialized:
             initData = s.recv(BUFFER_SIZE)
-            init = json.loads(initData)
+            db = json.loads(initData)
             s.send('ACK')
             initialized = True
         ready = raw_input("Type 'y' to begin")
         if ready == 'y':
             # Send IDS
-            IDS = json.dumps({ 'IDS': init['IDS'] })
+            IDS = json.dumps({ 'IDS': db['IDS'] })
             s.sendall(IDS)
 
             # Expect A, B, C
             data = s.recv(BUFFER_SIZE)
             abc = json.loads(data)
-            print "received data:", abc
+            
             # Calculate B' and check it B == B'
             # Calculate and send D
             D = verifyABC(abc)
@@ -61,9 +78,9 @@ try:
                 break
             dsend = json.dumps({ 'D': D })
             s.sendall(dsend)
-            print { 'ID': ID }
 
             # Calculate new IDS, Ks 1-4,
+            update_values()
 except Exception as e:
     print(e)
     s.close()
