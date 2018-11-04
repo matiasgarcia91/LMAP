eb623e716814ea5cac48dd3f842c695c269dc7e8
Pablo Pastor Martín <alu0100890839@ull.edu.es>
2018-11-04 14:33:28 +0100

    OOP, json with initial keys, UDP sockets, improved the way the protocol is imitated

--- LMAP.py
+++ LMAP.py
@@ -1,5 +1,6 @@
-import socket, sys, random, json
+import socket, sys, random, json, time
 
+'''
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -18,84 +19,108 @@ k2 = random.randint(0, 2**96 - 1)
 k3 = random.randint(0, 2**96 - 1)
 k4 = random.randint(0, 2**96 - 1)
 db = { "k1": k1, "k2": k2, "k3": k3, "k4": k4 }
+'''
 
-def calculateABC(IDS):
-    global db
-    k1 = db["k1"]
-    k2 = db["k2"]
-    k3 = db["k3"]
-    n1 = random.randint(0, 2**96 - 1)
-    n2 = random.randint(0, 2**96 - 1)
-    db['IDS'] = IDS
-    db['n1'] = n1
-    db['n2'] = n2
-    A = IDS ^ k1 ^ n1
-    B = (IDS | k2) + n1
-    C = IDS + k3 + n2
-    return [ A, B, C, n1, n2 ]
+class LmapReader:
 
-def decodeD(D):
-    global db
-    n1 = db['n1']
-    n2 = db['n2']
-    IDS = db['IDS']
-    ID = (D ^ n1 ^ n2) - IDS
-    return ID
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(("127.0.0.1", 3000))
+        json_data = open("initial_keys.json").read()
+        self.db = json.loads(json_data)
+        self.listeners = [("127.0.0.1", 3001),]
 
-def update_values(ID):
-    global db
-    IDS = db['IDS']
-    k1 = db['k1']
-    k2 = db['k2']
-    k3 = db['k3']
-    k4 = db['k4']
-    n1 = db['n1']
-    n2 = db['n2']
-    newIDS = (IDS + (n2 ^ k4)) ^ ID
-    newk1 = k1 ^ n2 ^ (k3 + ID)
-    newk2 = k2 ^ n2 ^ (k4 + ID)
-    newk3 = (k3 ^ n1) + (k1 ^ ID)
-    newk4 = (k4 ^ n1) + (k2 ^ ID)
-    db = { 'k1': newk1, 'k2': newk2, 'k3': newk3, 'k4': newk4, 'IDS': newIDS }
-    print ID
 
-while True:
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
-    try:
-        print >>sys.stderr, 'client connected:', client_address
-        initialized = False
+    def sendMsg(self, msg):
+        for addr in self.listeners:
+            self.socket.sendto(msg.encode(), addr)
+
+    def calculateABC(self, IDS):
+        k1 = self.db["k1"]
+        k2 = self.db["k2"]
+        k3 = self.db["k3"]
+        n1 = random.randint(0, 2**96 - 1)
+        n2 = random.randint(0, 2**96 - 1)
+        self.db['IDS'] = IDS
+        self.db['n1'] = n1
+        self.db['n2'] = n2
+        A = IDS ^ k1 ^ n1
+        B = (IDS | k2) + n1
+        C = IDS + k3 + n2
+        return [ A, B, C, n1, n2 ]
+
+    def decodeD(self, D):
+        n1 = self.db['n1']
+        n2 = self.db['n2']
+        IDS = self.db['IDS']
+        ID = (D ^ n1 ^ n2) - IDS
+        return ID
+
+    def update_values(self, ID):
+        IDS = self.db['IDS']
+        k1 = self.db['k1']
+        k2 = self.db['k2']
+        k3 = self.db['k3']
+        k4 = self.db['k4']
+        n1 = self.db['n1']
+        n2 = self.db['n2']
+        newIDS = (IDS + (n2 ^ k4)) ^ ID
+        newk1 = k1 ^ n2 ^ (k3 + ID)
+        newk2 = k2 ^ n2 ^ (k4 + ID)
+        newk3 = (k3 ^ n1) + (k1 ^ ID)
+        newk4 = (k4 ^ n1) + (k2 ^ ID)
+        self.db = { 'k1': newk1, 'k2': newk2, 'k3': newk3, 'k4': newk4, 'IDS': newIDS }
+        print(ID)
+
+    def run(self):
+        self.socket.sendto("listener".encode(), ("127.0.0.1", 3001))
         while True:
-            # Send initial IDS and Ks to tag - not part of the protocol but needed to share the
-            # same initial values between entities
-            if not initialized:
-                init = json.dumps({ 'IDS': IDS_init, 'k1': k1, 'k2': k2, 'k3': k3, 'k4': k4 })
-                connection.sendall(init)
-                ack = connection.recv(10000)
-                if ack == 'ACK': initialized = True
-                print 'initialized'
-            # Expect IDS
-            data = connection.recv(10000)
-            ids_recieved = json.loads(data)
+            '''
+            print >>sys.stderr, 'waiting for a connection'
+            connection, client_address = sock.accept()
+            '''
+            try:
+                self.sendMsg("hello")
+                '''
+                print >>sys.stderr, 'client connected:', client_address
+                initialized = False
+                '''
+                # Send initial IDS and Ks to tag - not part of the protocol but needed to share the
+                # same initial values between entities
+                '''
+                if not initialized:
+                    init = json.dumps({ 'IDS': IDS_init, 'k1': k1, 'k2': k2, 'k3': k3, 'k4': k4 })
+                    connection.sendall(init)
+                    ack = connection.recv(10000)
+                    if ack == 'ACK': initialized = True
+                    print('initialized')
+                '''
+                # Expect IDS
+                data, _ = self.socket.recvfrom(10000)
+                print(data.decode())
+                ids_recieved = json.loads(data.decode())
 
-            # Calculate A, B, C and send
-            todo = calculateABC(int(ids_recieved['IDS']))
-            abc = { 'A': todo[0], 'B': todo[1], 'C': todo[2] }
-            abcSerial = json.dumps(abc)
-            connection.sendall(abcSerial)
+                # Calculate A, B, C and send
+                todo = self.calculateABC(int(ids_recieved['IDS']))
+                abc = { 'A': todo[0], 'B': todo[1], 'C': todo[2] }
+                abcSerial = json.dumps(abc)
+                self.sendMsg(abcSerial)
 
-            # Expect D, extract ID
-            authreplyraw = connection.recv(10000)
-            authreply = json.loads(authreplyraw)
-            ID = decodeD(authreply['D'])
-            # Update IDS and Ks 1-4
-            update_values(ID)
-    except Exception as e:
-        print(e)
-        connection.close()
-    finally:
-        connection.close()
+                # Expect D, extract ID
+                authreplyraw, _ = self.socket.recvfrom(10000)
+                authreply = json.loads(authreplyraw.decode())
+                ID = self.decodeD(authreply['D'])
+                # Update IDS and Ks 1-4
+                self.update_values(ID)
+                
+                time.sleep(1.5)
+            except Exception as e:
+                print(e)
+                continue
 
-# A = IDS XOR K1 XOR n1 now the tag knows n1
-# B = (IDS OR K2) + n1 reader authentication
-# C = IDS + K3 + n2 the tag knows n2
+    # A = IDS XOR K1 XOR n1 now the tag knows n1
+    # B = (IDS OR K2) + n1 reader authentication
+    # C = IDS + K3 + n2 the tag knows n2
+
+reader = LmapReader()
+reader.run()
--- tag.py
+++ tag.py
@@ -1,95 +1,114 @@
  #!/usr/bin/env python
 import socket, sys, random, json
 
+'''
 TCP_IP = '0.0.0.0'
 TCP_PORT = 3001
 BUFFER_SIZE = 10000
 MESSAGE = "Hello, World!"
 ID = random.randint(0, 2**96 - 1)
-db = {}
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 initialized = False
+'''
+
+BUFFER_SIZE = 10000
+
+class LmapTag:
+
+    def __init__(self):
+        json_data = open("initial_keys.json").read()
+        self.db = json.loads(json_data)
+        self.db['IDS'] = random.randint(0, 2**96 - 1)
+        self.ID = random.randint(0, 2**96-1)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.bind(("127.0.0.1", 3001))
+        self.listeners = []
+
+    def receive(self):
+        msg, _ = self.s.recvfrom(BUFFER_SIZE)
+        print("joder"+msg.decode())
+        return msg.decode()
+        
+    def sendMsg(self, msg):
+        for addr in self.listeners:
+            self.s.sendto(msg.encode(), addr)
+
+    def verifyABC(self, abc):
+        A = abc['A']
+        C = abc['C']
+        IDS = self.db['IDS']
+        k1 = self.db['k1']
+        k2 = self.db['k2']
+        k3 = self.db['k3']
+        n1 = A ^ k1 ^ IDS
+        B2 = (IDS | k2) + n1
+        if B2 == abc['B']:
+            print('Reader authenticated')
+        else:
+            print('auth failed')
+        n2 = C - IDS - k3
+        self.db['n1'] = n1
+        self.db['n2'] = n2
+        D = (IDS + self.ID) ^ n1 ^ n2
+        print('verifyready')
+        print(self.db)
+        return D
+
+    def update_values(self):
+        IDS = self.db['IDS']
+        print('updatedstarted')
+        k1 = self.db['k1']
+        k2 = self.db['k2']
+        k3 = self.db['k3']
+        k4 = self.db['k4']
+        n1 = self.db['n1']
+        n2 = self.db['n2']
+        print('updatedadasd')
+        newIDS = (IDS + (n2 ^ k4)) ^ self.ID
+        newk1 = k1 ^ n2 ^ (k3 + self.ID)
+        newk2 = k2 ^ n2 ^ (k4 + self.ID)
+        newk3 = (k3 ^ n1) + (k1 ^ self.ID)
+        newk4 = (k4 ^ n1) + (k2 ^ self.ID)
+        self.db = { 'k1': newk1, 'k2': newk2, 'k3': newk3, 'k4': newk4, 'IDS': newIDS }
+
+    def run(self):
+            while True:
+                #try:
+                    msg, addr = self.s.recvfrom(BUFFER_SIZE)
+                    if(msg.decode() == "listener"):
+                        self.listeners.append(addr)
+                    elif(msg.decode() == "hello"):
+                        print("HI")
+                        # Send IDS
+                        IDS = json.dumps({ 'IDS': self.db['IDS'] })
+                        self.sendMsg(IDS)
+
+                        # Expect A, B, C
+                        data = self.receive()
+                        abc = json.loads(data)
+                        
+                        # Calculate B' and check it B == B'
+                        # Calculate and send D
+                        D = self.verifyABC(abc)
+                        if not D:
+                            print('ABC invalid')
+                            continue
+                        dsend = json.dumps({ 'D': D })
+                        self.sendMsg(dsend)
+
+                        # Calculate new IDS, Ks 1-4,
+                        self.update_values()
+                        '''except Exception as e:
+                            print(e)
+                            continue
+                        '''
 
 
-def verifyABC(abc):
-    global db
-    A = abc['A']
-    C = abc['C']
-    IDS = db['IDS']
-    k1 = db['k1']
-    k2 = db['k2']
-    k3 = db['k3']
-    n1 = A ^ k1 ^ IDS
-    B2 = (IDS | k2) + n1
-    if B2 == abc['B']:
-        print 'Reader authenticated'
-    else:
-        print 'auth failed'
-    n2 = C - IDS - k3
-    db['n1'] = n1
-    db['n2'] = n2
-    D = (IDS + ID) ^ n1 ^ n2
-    print 'verifyready'
-    print db
-    return D
+    # A = IDS XOR K1 XOR n1 now the tag knows n1
+    # B = (IDS OR K2) + n1 reader authentication
+    # C = IDS + K3 + n2 the tag knows n2
 
-def update_values():
-    global db
-    IDS = db['IDS']
-    print 'updatedstarted'
-    k1 = db['k1']
-    k2 = db['k2']
-    k3 = db['k3']
-    k4 = db['k4']
-    n1 = db['n1']
-    n2 = db['n2']
-    print 'updatedadasd'
-    newIDS = (IDS + (n2 ^ k4)) ^ ID
-    newk1 = k1 ^ n2 ^ (k3 + ID)
-    newk2 = k2 ^ n2 ^ (k4 + ID)
-    newk3 = (k3 ^ n1) + (k1 ^ ID)
-    newk4 = (k4 ^ n1) + (k2 ^ ID)
-    db = { 'k1': newk1, 'k2': newk2, 'k3': newk3, 'k4': newk4, 'IDS': newIDS }
-
-try:
-    while True:
-        if not initialized:
-            initData = s.recv(BUFFER_SIZE)
-            db = json.loads(initData)
-            s.send('ACK')
-            initialized = True
-        ready = raw_input("Type 'y' to begin")
-        if ready == 'y':
-            # Send IDS
-            IDS = json.dumps({ 'IDS': db['IDS'] })
-            s.sendall(IDS)
-
-            # Expect A, B, C
-            data = s.recv(BUFFER_SIZE)
-            abc = json.loads(data)
-            
-            # Calculate B' and check it B == B'
-            # Calculate and send D
-            D = verifyABC(abc)
-            if not D:
-                print 'ABC invalid'
-                break
-            dsend = json.dumps({ 'D': D })
-            s.sendall(dsend)
-
-            # Calculate new IDS, Ks 1-4,
-            update_values()
-except Exception as e:
-    print(e)
-    s.close()
-    initialized = False
-finally:
-    s.close()
-    initialized = False
-
-
-# A = IDS XOR K1 XOR n1 now the tag knows n1
-# B = (IDS OR K2) + n1 reader authentication
-# C = IDS + K3 + n2 the tag knows n2
+tag = LmapTag()
+tag.run()
