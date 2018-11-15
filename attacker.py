97974854487377a438d83de0c6569dd5b5f12663
Pablo Pastor <alu0100890839@ull.edu.es>
2018-11-15 18:00:48 +0100

    Minor changes tht will make it easier to keep coding the attack based on 'breaking LMAP' paper

--- attacker.py
+++ attacker.py
@@ -32,23 +32,29 @@ class LmapAttacker:
                     db[message] = BitVector(size=96, intVal=value)
                 return db
 
-    def get_bits(self, seen, index, it, carry, ID):
-        aux = seen[it]
-        aux["n1"][index] = aux["B"][index] ^ 1
-        aux["K1"][index] = aux["A"][index] ^ aux["n1"][index] ^ 1
-        aux["K4"][index] = seen[it+1]["IDS"][index] ^ aux["D"][index] ^ aux["n1"][index] 
-        n22 = seen[it+1]["IDS"][index] ^ seen[it+1]["C"][index] ^ aux["C"][index] ^ aux["D"][index] ^ aux["K1"][index] 
-        aux["K2"][index] = seen[it+2]["IDS"][index] ^ seen[it+1]["IDS"][index] ^ n22 ^ aux["K4"][index] ^ aux["n1"][index]
-        n12 = aux["B"][index] ^ (seen[it+1]["IDS"][index] | (aux["K2"][index] ^ aux["K4"][index] ^ aux["D"][index] ^ aux["n1"][index]))
-        ID[index] = seen[it+1]["IDS"][index] ^ seen[it+1]["D"][index] ^ n12 ^ n22
-        aux["K3"][index] = aux["C"][index] ^ aux["n2"][index] ^ 1
-        aux["n2"][index] = ID[index] ^ aux["n1"][index] ^ aux["D"][index] ^ 1
+    def carry(a,b,c):
+        return (a & b) | ((a | b) & c)
 
+    def get_bits(self, index, it, carry):
+        
+        aux = seen[it]
+
+        def get(name, round_dif=0, offset=0):
+            return self.seen[it+round_dif][name][index + offset] 
+
+        aux["n1"][index] = get("B") ^ get("IDS")
+        aux["K1"][index] = get("A") ^ get("n1") ^ get("IDS")
+        aux["K4"][index] = get("IDS", 1) ^ get("D") ^ get("n1") 
+        n22 = get("IDS",1) ^ get("C",1) ^ get("C") ^ get("D") ^ get("K1") 
+        aux["K2"][index] = get("IDS",2) ^ get("IDS",1) ^ n22 ^ get("K4") ^ get("n1")
+        n12 = get("B") ^ (get("IDS",1) | (get("K2") ^ get("K4") ^ get("D") ^ get("n1")))
+        ID[index] = get("IDS", 1) ^ get("IDS", 1) ^ n12 ^ n22
+        aux["K3"][index] = get("C") ^ get("n2") ^ get("IDS")
+        aux["n2"][index] = ID[index] ^ get("n1") ^ get("D") ^ get("IDS")
 
 
 
     def reveal(self, seen, where_set):
-        ID = BitVector(size=96)
         for auxround in seen:
             auxround["K1"] = BitVector(size=96)
             auxround["K2"] = BitVector(size=96)
@@ -87,6 +93,10 @@ class LmapAttacker:
         data = self.listen_round()
         seen.append(data)
         count+=2
+
+        self.seen = seen
+        self.where_set = where_set
+        self.ID = BitVector(size=96)
 
 
     
